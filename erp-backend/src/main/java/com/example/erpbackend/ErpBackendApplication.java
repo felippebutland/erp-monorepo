1de34cb985e85222d4150409b6d968e81c5b14a6
felippebutland <butlandfelippe@gmail.com>
2023-04-14 21:05:00 -0300

    feat: change the java to maven

--- erp-backend/src/main/java/com/example/erpbackend/ErpBackendApplication.java
+++ erp-backend/src/main/java/com/example/erpbackend/ErpBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ErpBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ErpBackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ErpBackendApplication.class, args);
+	}
 
 }
