8b57d6cd8989b0aa05830b8c9dd9c79466521432
Alexandre Zanella <srjanela@gmail.com>
2023-05-19 07:04:06 -0300

    fix: adjustments to logger repository

--- erp-spring/src/main/java/com/backend/erpspring/repository/LoggerRepository.java
+++ erp-spring/src/main/java/com/backend/erpspring/repository/LoggerRepository.java
@@ -2,8 +2,12 @@ package com.backend.erpspring.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.backend.erpspring.Entities.LoggerEntity;
 
-public class LoggerRepository extends JpaRepository<LoggerEntity, UUID> {
+@Repository
+public interface LoggerRepository extends JpaRepository<LoggerEntity, UUID> {
     
 }
