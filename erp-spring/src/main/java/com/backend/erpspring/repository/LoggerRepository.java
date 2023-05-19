b497aaacffd2b8d8a75731c55fed716dd598cecf
Alexandre Zanella <srjanela@gmail.com>
2023-05-19 07:00:43 -0300

    feat: implementation of local loggers

---
+++ erp-spring/src/main/java/com/backend/erpspring/repository/LoggerRepository.java
@@ -0,0 +1,9 @@
+package com.backend.erpspring.repository;
+
+import java.util.UUID;
+
+import com.backend.erpspring.Entities.LoggerEntity;
+
+public class LoggerRepository extends JpaRepository<LoggerEntity, UUID> {
+    
+}
